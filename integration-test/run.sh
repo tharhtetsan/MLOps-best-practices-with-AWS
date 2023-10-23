b9655ce0387691ac0d9b7ffc77416f3d32ce1ed9
Thar Htet San <tharhtet1234@gmail.com>
2023-10-23 14:15:51 +0700

    Update run.sh

--- integration-test/run.sh
+++ integration-test/run.sh
@@ -4,14 +4,7 @@ if [[ -z "${GITHUB_ACTIONS}" ]]; then
   cd "$(dirname "$0")"
 fi
 
-
-echo "${GITHUB_ACTIONS}"
-echo "$(dirname "$0")"
-echo "##############################################"
-
-
-
-if [ "${LOCAL_IMAGE_NAME}" == "" ]; then
+if [ "${LOCAL_IMAGE_NAME}" == "" ]; then 
     LOCAL_TAG=`date +"%Y-%m-%d-%H-%M"`
     export LOCAL_IMAGE_NAME="stream-model-duration:${LOCAL_TAG}"
     echo "LOCAL_IMAGE_NAME is not set, building a new image with tag ${LOCAL_IMAGE_NAME}"
@@ -30,7 +23,6 @@ aws --endpoint-url=http://localhost:4566 \
     kinesis create-stream \
     --stream-name ${PREDICTIONS_STREAM_NAME} \
     --shard-count 1
-
 
 pipenv run python test_docker.py
 
