a51222955c24182723bc434332371f74428bd7ac
Thar Htet San <tharhtet1234@gmail.com>
2023-10-23 15:07:06 +0700

    Update run.sh

--- integration-test/run.sh
+++ integration-test/run.sh
@@ -8,7 +8,7 @@ if [ "${LOCAL_IMAGE_NAME}" == "" ]; then
     LOCAL_TAG=`date +"%Y-%m-%d-%H-%M"`
     export LOCAL_IMAGE_NAME="stream-model-duration:${LOCAL_TAG}"
     echo "LOCAL_IMAGE_NAME is not set, building a new image with tag ${LOCAL_IMAGE_NAME}"
-    docker build -t ${LOCAL_IMAGE_NAME} ..
+    docker build -t ${LOCAL_IMAGE_NAME} .
 else
     echo "no need to build image ${LOCAL_IMAGE_NAME}"
 fi
@@ -31,7 +31,7 @@ echo ls -la
 echo $PWD
 
 
-pipenv run python test_docker.py
+pipenv run python integration-test/test_docker.py
 
 ERROR_CODE=$?
 
@@ -42,7 +42,7 @@ if [ ${ERROR_CODE} != 0 ]; then
 fi
 
 
-pipenv run python test_kinesis.py
+pipenv run python integration-test/test_kinesis.py
 
 ERROR_CODE=$?
 
